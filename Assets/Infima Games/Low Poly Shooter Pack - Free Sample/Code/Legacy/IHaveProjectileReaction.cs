5ab5d741283c1582b4b9ef1cf4e4af3fa90585ea
Danylo <dbashinskiy@miem.hse.ru>
2024-08-25 18:11:02 +0300

    Task3_1 completed

--- Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Legacy/IHaveProjectileReaction.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infima_Games.Low_Poly_Shooter_Pack___Free_Sample.Code.Legacy
-{
-    public interface IHaveProjectileReaction
-    {
-        public void React() { }
-    }
-}
--- Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Legacy/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Infima_Games.Low_Poly_Shooter_Pack___Free_Sample.Code.Legacy
-{
-    public interface IInteractable
-    {
-        public void Interact();
-        public string GetInteractionText();
-    }
-}
